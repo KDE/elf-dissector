3964a8e366d4e78b51636b2f5270ca8b84a5d5c9
Volker Krause <vkrause@kde.org>
2013-04-14 19:04:57 +0200

    Expand the demangler to handle everything we encounter in Qt libs.

--- demangle/demangler.cpp
+++ demangle/demangler.cpp
@@ -46,6 +46,14 @@ struct demangle_operator_info
     int args;
 };
 
+struct demangle_builtin_type_info {
+    const char *name;
+    int len;
+    const char *java_name;
+    int java_len;
+    /*enum d_builtin_type_print*/ int print;
+};
+
 void Demangler::handleNameComponent(demangle_component* component, QVector< QByteArray >& nameParts)
 {
     // TODO: complete the component types
@@ -57,6 +65,14 @@ void Demangler::handleNameComponent(demangle_component* component, QVector< QByt
             handleNameComponent(component->u.s_binary.left, nameParts);
             handleNameComponent(component->u.s_binary.right, nameParts);
             break;
+        case DEMANGLE_COMPONENT_LOCAL_NAME:
+            handleNameComponent(component->u.s_binary.left, nameParts);
+            handleNameComponent(component->u.s_binary.right, nameParts);
+            break;
+        case DEMANGLE_COMPONENT_TYPED_NAME:
+            handleNameComponent(component->u.s_binary.left, nameParts);
+            // TODO: right side relevant?
+            break;
         case DEMANGLE_COMPONENT_TEMPLATE:
             handleNameComponent(component->u.s_binary.left, nameParts);
             // TODO handle template args?
@@ -65,18 +81,109 @@ void Demangler::handleNameComponent(demangle_component* component, QVector< QByt
             // TODO: do we need to consider u.s_ctor.kind?
             handleNameComponent(component->u.s_ctor.name, nameParts);
             break;
-        case DEMANGLE_COMPONENT_VTABLE:
-            nameParts.push_back("vtable");
-            break;
         case DEMANGLE_COMPONENT_DTOR:
             // TODO: do we need to consider u.s_dtor.kind?
             handleNameComponent(component->u.s_dtor.name, nameParts);
+            break;
+        case DEMANGLE_COMPONENT_VTABLE:
+            handleNameComponent(component->u.s_binary.left, nameParts);
+            nameParts.push_back("vtable");
+            break;
+        case DEMANGLE_COMPONENT_TYPEINFO:
+            handleNameComponent(component->u.s_binary.left, nameParts);
+            nameParts.push_back("typeinfo");
+            break;
+        case DEMANGLE_COMPONENT_TYPEINFO_NAME:
+            handleNameComponent(component->u.s_binary.left, nameParts);
+            nameParts.push_back("typeinfo name");
+            break;
+        case DEMANGLE_COMPONENT_THUNK:
+            handleNameComponent(component->u.s_binary.left, nameParts);
+            nameParts.push_back("thunk");
+            break;
+        case DEMANGLE_COMPONENT_VIRTUAL_THUNK:
+            handleNameComponent(component->u.s_binary.left, nameParts);
+            nameParts.push_back("virtual thunk");
+            break;
+        case DEMANGLE_COMPONENT_COVARIANT_THUNK:
+            handleNameComponent(component->u.s_binary.left, nameParts);
+            nameParts.push_back("covariant thunk");
+            break;
+        case DEMANGLE_COMPONENT_GUARD:
+            handleNameComponent(component->u.s_binary.left, nameParts);
+            nameParts.push_back("guard");
+            break;
+        case DEMANGLE_COMPONENT_REFTEMP:
+            handleNameComponent(component->u.s_binary.left, nameParts);
+            nameParts.push_back("reference temporary");
+            break;
+        case DEMANGLE_COMPONENT_HIDDEN_ALIAS:
+            handleNameComponent(component->u.s_binary.left, nameParts);
+            nameParts.push_back("hidden alias");
+            break;
+        case DEMANGLE_COMPONENT_SUB_STD:
+            nameParts.push_back(QByteArray(component->u.s_name.s, component->u.s_name.len));
+            break;
+        case DEMANGLE_COMPONENT_RESTRICT:
+            handleNameComponent(component->u.s_binary.left, nameParts);
+            nameParts.last().append(" restrict");
+            break;
+        case DEMANGLE_COMPONENT_VOLATILE:
+            handleNameComponent(component->u.s_binary.left, nameParts);
+            nameParts.last().append(" volatile");
+            break;
+        case DEMANGLE_COMPONENT_CONST:
+            handleNameComponent(component->u.s_binary.left, nameParts);
+            nameParts.last().append(" const");
+            break;
+        case DEMANGLE_COMPONENT_RESTRICT_THIS:
+            handleNameComponent(component->u.s_binary.left, nameParts);
+            nameParts.last().append(" restrict");
+            break;
+        case DEMANGLE_COMPONENT_VOLATILE_THIS:
+            handleNameComponent(component->u.s_binary.left, nameParts);
+            nameParts.last().append(" volatile");
+            break;
+        case DEMANGLE_COMPONENT_CONST_THIS:
+            handleNameComponent(component->u.s_binary.left, nameParts);
+            nameParts.last().append(" const");
+            break;
+        case DEMANGLE_COMPONENT_POINTER:
+            handleNameComponent(component->u.s_binary.left, nameParts);
+            nameParts.last().append("*");
+            break;
+        case DEMANGLE_COMPONENT_REFERENCE:
+            handleNameComponent(component->u.s_binary.left, nameParts);
+            nameParts.last().append("&");
+            break;
+        case DEMANGLE_COMPONENT_RVALUE_REFERENCE:
+            handleNameComponent(component->u.s_binary.left, nameParts);
+            nameParts.last().append("&&");
+            break;
+        case DEMANGLE_COMPONENT_BUILTIN_TYPE:
+            nameParts.push_back(QByteArray(component->u.s_builtin.type->name, component->u.s_builtin.type->len));
+            break;
+        case DEMANGLE_COMPONENT_PTRMEM_TYPE:
+            handleNameComponent(component->u.s_binary.left, nameParts);
+            handleNameComponent(component->u.s_binary.right, nameParts);
             break;
         case DEMANGLE_COMPONENT_OPERATOR:
             nameParts.push_back(QByteArray("operator") + QByteArray(component->u.s_operator.op->name, component->u.s_operator.op->len));
             break;
         case DEMANGLE_COMPONENT_EXTENDED_OPERATOR:
             handleNameComponent(component->u.s_extended_operator.name, nameParts);
+            break;
+        case DEMANGLE_COMPONENT_CAST:
+            // TODO we probably want to mention this is a cast?
+            handleNameComponent(component->u.s_binary.left, nameParts);
+            break;
+        case DEMANGLE_COMPONENT_LAMBDA:
+            // TODO what's in here??
+            nameParts.push_back("lambda");
+            break;
+        case DEMANGLE_COMPONENT_UNNAMED_TYPE:
+            // TODO what's in here?
+            nameParts.push_back("unnamed");
             break;
         default:
             qDebug() << Q_FUNC_INFO << "unhandled component type" << component->type;
