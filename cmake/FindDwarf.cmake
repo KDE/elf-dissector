3f03625987b15e7df2cecfdd46214f3c81de2c33
Volker Krause <vkrause@kde.org>
2015-02-05 19:47:56 +0100

    Update FindDwarf to use imported targets.

--- cmake/FindDwarf.cmake
+++ cmake/FindDwarf.cmake
@@ -1,12 +1,20 @@
 find_path(Dwarf_INCLUDE_DIR libdwarf/libdwarf.h)
 
 find_library(Dwarf_LIBRARY NAMES dwarf)
-find_library(Elf_LIBRARY NAMES elf)
-set(Dwarf_LIBS ${Dwarf_LIBRARY} ${Elf_LIBRARY})
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(Dwarf DEFAULT_MSG Dwarf_LIBRARY Dwarf_INCLUDE_DIR)
 
+if(DWARF_FOUND AND NOT TARGET Dwarf::Dwarf)
+    add_library(Dwarf::Dwarf UNKNOWN IMPORTED)
+    set_target_properties(Dwarf::Dwarf PROPERTIES
+        IMPORTED_LOCATION "${Dwarf_LIBRARY}"
+        INTERFACE_INCLUDE_DIRECTORIES "${Dwarf_INCLUDE_DIR}"
+    )
+endif()
+
+mark_as_advanced(Dwarf_LIBRARY Elf_LIBRARY Dwarf_INCLUDE_DIR)
+
 include(FeatureSummary)
 set_package_properties(libdwarf-devel PROPERTIES URL http://www.dwarfstd.org/
-  DESCRIPTION "DWARF debug information library")
+    DESCRIPTION "DWARF debug information library")
