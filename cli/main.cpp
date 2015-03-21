297e886151a2dba2f2d34785a25bb482fde5fa84
Volker Krause <vkrause@kde.org>
2015-03-21 13:42:20 +0100

    Add CLI version of the structure packing checker.

---
+++ cli/main.cpp
@@ -0,0 +1,48 @@
+/*
+    Copyright (C) 2015 Volker Krause <vkrause@kde.org>
+
+    This program is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Library General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or (at your
+    option) any later version.
+
+    This program is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public
+    License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <checks/structurepackingcheck.h>
+
+#include <elf/elffileset.h>
+
+#include <QCoreApplication>
+#include <QCommandLineParser>
+
+int main(int argc, char** argv)
+{
+    QCoreApplication::setApplicationName("ELF Dissector");
+    QCoreApplication::setOrganizationName("KDAB");
+    QCoreApplication::setOrganizationDomain("kdab.com");
+    QCoreApplication::setApplicationVersion("0.0.1");
+
+    QCoreApplication app(argc, argv);
+
+    QCommandLineParser parser;
+    parser.addHelpOption();
+    parser.addVersionOption();
+    parser.addPositionalArgument("elf", "ELF library to open", "<elf>");
+    parser.process(app);
+
+    for (const auto &fileName : parser.positionalArguments()) {
+        ElfFileSet set;
+        set.addFile(fileName);
+        StructurePackingCheck checker(&set);
+        checker.checkAll(set.file(0)->dwarfInfo());
+    }
+
+    return 0;
+}
