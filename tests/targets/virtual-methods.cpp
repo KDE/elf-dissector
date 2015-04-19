bcf25236a8781c2a1ab68f4badd87b1f071807d5
Volker Krause <vkrause@kde.org>
2015-04-19 15:02:37 +0200

    Add test target for virtual methods.

---
+++ tests/targets/virtual-methods.cpp
@@ -0,0 +1,38 @@
+/*
+    Copyright (C) 2015 Volker Krause <vkrause@kde.org>
+
+    This program is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Library General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or (at your
+    option) any later version.
+
+    This program is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public
+    License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+struct Base {
+    virtual void pure() = 0;
+    virtual void baseOnly() {};
+    virtual void overridden() {};
+};
+
+struct Derived : public Base {
+    void pure() override {};
+    void overridden() override {};
+};
+
+int main (int, char**)
+{
+    // make sure the structures aren't optimized away by the compiler
+    unsigned long long dummy = 0;
+    #define USED(StructType) { StructType s; dummy += (unsigned long long)&s; }
+
+    USED(Derived);
+
+    return dummy;
+}
