82dbf1aa43ba9ee45c590e18e34ebe38dc5ee7e9
Volker Krause <vkrause@kde.org>
2015-04-26 10:39:28 +0200

    Add more missing default parameters.

--- ui/model/elfmodel.h
+++ ui/model/elfmodel.h
@@ -42,9 +42,9 @@ public:
 
     void setFileSet(ElfFileSet* fileSet);
 
-    QVariant data(const QModelIndex& index, int role) const override;
-    int columnCount(const QModelIndex& parent) const override;
-    int rowCount(const QModelIndex& parent) const override;
+    QVariant data(const QModelIndex& index, int role = Qt::DisplayRole) const override;
+    int columnCount(const QModelIndex& parent = QModelIndex()) const override;
+    int rowCount(const QModelIndex& parent = QModelIndex()) const override;
     QModelIndex parent(const QModelIndex& child) const override;
     QModelIndex index(int row, int column, const QModelIndex& parent) const override;
     QVariant headerData(int section, Qt::Orientation orientation, int role = Qt::DisplayRole) const override;
