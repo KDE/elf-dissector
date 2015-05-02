80e1c809610cd4bc292e39aca58b57dd4e2964f1
Volker Krause <vkrause@kde.org>
2015-05-02 18:17:56 +0200

    Add access to the file type.

--- lib/elf/elfheader.h
+++ lib/elf/elfheader.h
@@ -24,7 +24,7 @@
 class ElfHeader
 {
 public:
-//     virtual uint16_t type() const = 0;
+    virtual uint16_t type() const = 0;
     virtual uint16_t machine() const = 0;
 //     virtual uint32_t version() const = 0;
 //     virtual uint64_t entryPoint() const = 0;
@@ -45,6 +45,7 @@ class ElfHeaderImpl : public ElfHeader
 public:
     explicit inline ElfHeaderImpl(unsigned char *data) : m_hdr(reinterpret_cast<T*>(data)) {}
 
+    inline uint16_t type() const override { return m_hdr->e_type; }
     inline uint16_t machine() const override { return m_hdr->e_machine; }
     inline uint64_t sectionHeaderTableOffset() const override { return m_hdr->e_shoff; }
     inline uint16_t sectionHeaderEntrySize() const override { return m_hdr->e_shentsize; }
