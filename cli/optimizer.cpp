e5ac8f4edaa861bf0b2a8cf2224fd71d3c9ea4f0
Volker Krause <vkrause@kde.org>
2015-05-17 15:52:07 +0200

    Add CLI version for the optimizer.
    Tested on KMail (without --as-needed): 30% (lazy binding) to 45% (immediate
    binding) startup improvement :)

---
+++ cli/optimizer.cpp
@@ -0,0 +1,52 @@
+/*
+    Copyright (C) 2015 Volker Krause <vkrause@kde.org>
+
+    This program is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Library General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or (at your
+    option) any later version.
+
+    This program is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public
+    License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <config-elf-dissector-version.h>
+
+#include <optimizers/dependencysorter.h>
+
+#include <elf/elffileset.h>
+
+#include <QCoreApplication>
+#include <QCommandLineParser>
+
+int main(int argc, char** argv)
+{
+    QCoreApplication::setApplicationName("ELF Optimizer");
+    QCoreApplication::setOrganizationName("KDE");
+    QCoreApplication::setOrganizationDomain("kde.org");
+    QCoreApplication::setApplicationVersion(ELF_DISSECTOR_VERSION_STRING);
+
+    QCoreApplication app(argc, argv);
+
+    QCommandLineParser parser;
+    parser.addHelpOption();
+    parser.addVersionOption();
+    parser.addPositionalArgument("elf", "ELF library to optimize", "<elf>");
+    parser.process(app);
+
+    DependencySorter optimizer;
+    for (const auto &fileName : parser.positionalArguments()) {
+        ElfFileSet set;
+        set.addFile(fileName);
+        if (set.size() == 0)
+            continue;
+        optimizer.sortDtNeeded(&set);
+    }
+
+    return 0;
+}
