13fccec6aa9f15017a20efb463b67c9403ced8ad
Volker Krause <vkrause@kde.org>
2015-05-28 18:11:16 +0200

    CLI version of the unused dependency checker.
    Easier to measure if code changes have any effect this way.

---
+++ cli/depcheck.cpp
@@ -0,0 +1,52 @@
+/*
+    Copyright (C) 2015 Volker Krause <vkrause@kde.org>
+
+    This program is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Library General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or (at your
+    option) any later version.
+
+    This program is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public
+    License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <config-elf-dissector-version.h>
+
+#include <checks/unuseddependenciescheck.h>
+
+#include <elf/elffileset.h>
+
+#include <QCoreApplication>
+#include <QCommandLineParser>
+
+int main(int argc, char** argv)
+{
+    QCoreApplication::setApplicationName("ELF Dissector");
+    QCoreApplication::setOrganizationName("KDE");
+    QCoreApplication::setOrganizationDomain("kde.org");
+    QCoreApplication::setApplicationVersion(ELF_DISSECTOR_VERSION_STRING);
+
+    QCoreApplication app(argc, argv);
+
+    QCommandLineParser parser;
+    parser.addHelpOption();
+    parser.addVersionOption();
+    parser.addPositionalArgument("elf", "ELF library to open", "<elf>");
+    parser.process(app);
+
+    for (const auto &fileName : parser.positionalArguments()) {
+        ElfFileSet set;
+        set.addFile(fileName);
+        if (set.size() == 0)
+            continue;
+        UnusedDependenciesCheck checker;
+        checker.checkFileSet(&set);
+    }
+
+    return 0;
+}
