42d30c2e40c0d26fa1ed0659d3b11e962ae2b9b8
Volker Krause <vkrause@kde.org>
2015-08-06 10:47:12 +0200

    First attempt on an unreferenced symbol finder.
    Useful to identify unnecessarily exported symbols or dead code.

---
+++ lib/checks/deadcodefinder.cpp
@@ -0,0 +1,91 @@
+/*
+    Copyright (C) 2015 Volker Krause <vkrause@kde.org>
+
+    This program is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Library General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or (at your
+    option) any later version.
+
+    This program is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public
+    License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "deadcodefinder.h"
+#include <elf/elffileset.h>
+#include <elf/elfsymboltablesection.h>
+#include <elf/elfhashsection.h>
+#include <elf/elfheader.h>
+
+#include <demangle/demangler.h>
+
+#include <elf.h>
+
+#include <iostream>
+
+DeadCodeFinder::DeadCodeFinder() = default;
+DeadCodeFinder::~DeadCodeFinder() = default;
+
+void DeadCodeFinder::findUnusedSymbols(ElfFileSet* fileSet)
+{
+    m_fileSet = fileSet;
+
+    for (int i = 0; i < m_fileSet->size(); ++i) {
+        scanUsage(m_fileSet->file(i));
+    }
+}
+
+void DeadCodeFinder::scanUsage(ElfFile* file)
+{
+    std::cerr << "Scanning " << qPrintable(file->displayName()) << "..." << std::endl;
+    for (int i = 0; i < m_fileSet->size(); ++i) {
+        auto otherFile = m_fileSet->file(i);
+        auto symTab = file->hash()->linkedSection<ElfSymbolTableSection>();
+        if (!symTab)
+            continue;
+        for (uint j = 0; j < symTab->header()->entryCount(); ++j) {
+            auto localSym = symTab->entry(j);
+            if (localSym->size() > 0)
+                continue;
+            auto sym = otherFile->hash()->lookup(localSym->name());
+            if (!sym)
+                continue;
+            m_usedSymbols[otherFile].insert(sym);
+        }
+    }
+}
+
+void DeadCodeFinder::dumpResults()
+{
+    for (int i = 0; i < m_fileSet->size(); ++i) {
+        auto file = m_fileSet->file(i);
+        // this only makes sense for libraries
+        if (file->header()->type() == ET_EXEC)
+            continue;
+
+        std::cout << "Unreferenced exported symbols in " << qPrintable(file->displayName()) << ":" << std::endl;
+        dumpResultsForFile(file);
+        std::cout << std::endl;
+    }
+}
+
+void DeadCodeFinder::dumpResultsForFile(ElfFile* file)
+{
+    const auto usedSyms = m_usedSymbols.value(file);
+    const auto symTab = file->hash()->linkedSection<ElfSymbolTableSection>();
+    if (!symTab)
+        return;
+
+    for (uint i = 0; i < symTab->header()->entryCount(); ++i) {
+        auto sym = symTab->entry(i);
+        if (sym->size() == 0 || sym->bindType() != STB_GLOBAL || sym->visibility() != STV_DEFAULT)
+            continue;
+        if (usedSyms.contains(sym))
+            continue;
+        std::cout << Demangler::demangleFull(sym->name()).constData() << std::endl;
+    }
+}
---
+++ lib/checks/deadcodefinder.h
@@ -0,0 +1,49 @@
+/*
+    Copyright (C) 2015 Volker Krause <vkrause@kde.org>
+
+    This program is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Library General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or (at your
+    option) any later version.
+
+    This program is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public
+    License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef DEADCODEFINDER_H
+#define DEADCODEFINDER_H
+
+#include <QHash>
+#include <QSet>
+
+class ElfFileSet;
+class ElfFile;
+class ElfSymbolTableEntry;
+
+
+/** Identify unused exported symbols in a set of ELF objects. */
+class DeadCodeFinder
+{
+public:
+    DeadCodeFinder();
+    ~DeadCodeFinder();
+
+    void findUnusedSymbols(ElfFileSet *fileSet);
+
+    void dumpResults();
+
+private:
+    void scanUsage(ElfFile *file);
+
+    void dumpResultsForFile(ElfFile *file);
+
+    ElfFileSet *m_fileSet = nullptr;
+    QHash<ElfFile*, QSet<ElfSymbolTableEntry*>> m_usedSymbols;
+};
+
+#endif // DEADCODEFINDER_H
