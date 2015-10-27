e602de7a8f480a5da306a04a52534f6b178789b9
Volker Krause <vkrause@kde.org>
2015-10-27 20:24:07 +0100

    Add a test target exporting versioned symbols.

---
+++ tests/targets/versioned-symbols.c
@@ -0,0 +1,23 @@
+/*
+    Copyright (C) 2015 Volker Krause <vkrause@kde.org>
+
+    This program is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Library General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or (at your
+    option) any later version.
+
+    This program is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public
+    License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+__asm__(".symver function1, function@VER1");
+int function1() { return 1; }
+
+__asm__(".symver function2, function@@VER2");
+int function2() { return 2; }
