23844c5c046a5fcd976565426e3ab99174f97f82
Volker Krause <vkrause@kde.org>
2019-06-19 16:49:18 +0200

    Fix PLT/GOT offset for AArch64

--- lib/elf/elfpltentry.cpp
+++ lib/elf/elfpltentry.cpp
@@ -18,6 +18,10 @@
 #include "elfpltentry.h"
 #include "elfpltsection.h"
 #include "elfgotsection.h"
+#include "elffile.h"
+#include "elfheader.h"
+
+#include <elf.h>
 
 ElfPltEntry::ElfPltEntry(const ElfPltEntry&) = default;
 
@@ -52,6 +56,13 @@ uint64_t ElfPltEntry::size() const
 ElfGotEntry* ElfPltEntry::gotEntry() const
 {
     if (!m_index)
-        return nullptr; // see i386/x86_64 psABI documentation for content of the first entry
-    return m_section->gotSection()->entry(m_index + 2);
+        return nullptr;
+
+    // see i386/x86_64 psABI documentation for content of the first entry
+    // for AArch64 this is experimentally determined
+    if (m_section->file()->header()->machine() == EM_AARCH64) {
+        return m_section->gotSection()->entry(m_index + 1);
+    } else {
+        return m_section->gotSection()->entry(m_index + 2);
+    }
 }
