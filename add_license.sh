06c79ecbf573100fc4639b87407c71915eb5ac6b
Volker Krause <vkrause@kde.org>
2019-06-22 11:14:47 +0200

    Add license file

--- add_license.sh
+++ add_license.sh
@@ -21,7 +21,7 @@ find "$@" -name '*.h' -o -name '*.cpp' -o -name '*.qml' -o -name '*.c' | grep -v
     License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
 EOF
