5226a7cae19d37fb55cd6e5c56160eb3445fb82f
Adriaan de Groot <groot@kde.org>
2021-05-10 22:32:38 +0200

    Printers: group DF_1_* symbols by availability

--- src/lib/printers/dynamicsectionprinter.cpp
+++ src/lib/printers/dynamicsectionprinter.cpp
@@ -31,23 +31,27 @@ static const LookupTableEntry<uint64_t> dt_flag_infos[] {
 };
 
 static const LookupTableEntry<uint64_t> dt_flag_1_infos[] {
-    { DF_1_NOW, "set RTLD_NOW for this object" },
     { DF_1_GLOBAL, "set RTLD_GLOBAL for this object" },
-    { DF_1_GROUP, "set RTLD_GROUP for this object" },
     { DF_1_NODELETE, "set RTLD_NODELETE for this object" },
     { DF_1_LOADFLTR, "trigger filtee loading at runtime" },
-    { DF_1_INITFIRST, "set RTLD_INITFIRST for this object" },
     { DF_1_NOOPEN, "set RTLD_NOOPEN for this object" },
     { DF_1_ORIGIN, "$ORIGIN must be handled" },
-    { DF_1_DIRECT, "direct binding enabled" },
-    { DF_1_TRANS, "DF_1_TRANS" },
     { DF_1_INTERPOSE, "object is used to interpose" },
     { DF_1_NODEFLIB, "ignore default lib search path" },
+#ifdef DF_1_NOW
+    // Presumably GLIBC-dwarf.h defines, not available on FreeBSD
+    { DF_1_NOW, "set RTLD_NOW for this object" },
+    { DF_1_GROUP, "set RTLD_GROUP for this object" },
+    { DF_1_INITFIRST, "set RTLD_INITFIRST for this object" },
+    { DF_1_DIRECT, "direct binding enabled" },
+    { DF_1_TRANS, "DF_1_TRANS" },
     { DF_1_NODUMP, "object cannot be dldump'ed" },
     { DF_1_CONFALT, "configuration alternative created" },
     { DF_1_ENDFILTEE, "filtee terminates filters search" },
     { DF_1_DISPRELDNE, "disp reloc applied at build time" },
     { DF_1_DISPRELPND, "disp reloc applied at run-time" },
+#endif
+#ifdef __GLIBC_PREREQ
 #if __GLIBC_PREREQ(2, 17)
     { DF_1_NODIRECT, "object has no-direct binding" },
     { DF_1_IGNMULDEF, "DF_1_IGNMULDEF" },
@@ -58,6 +62,7 @@ static const LookupTableEntry<uint64_t> dt_flag_1_infos[] {
     { DF_1_SYMINTPOSE, "Object has individual interposers" },
     { DF_1_GLOBAUDIT, "Global auditing required" },
     { DF_1_SINGLETON, "Singleton symbols are used" }
+#endif
 #endif
 };
 
