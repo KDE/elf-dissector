3f7005d9bd287a407a75ae4c9081ad2e9342db78
Adriaan de Groot <groot@kde.org>
2021-05-12 00:17:29 +0200

    Autotests: distinguish FreeBSD from Linux ABI tests
    .. part of this might be Clang vs. gcc as well.

--- tests/auto/elfnotesectiontest.cpp
+++ tests/auto/elfnotesectiontest.cpp
@@ -27,13 +27,9 @@
 class ElfNoteSectionTest : public QObject
 {
     Q_OBJECT
-private slots:
-    void testABISection()
-    {
-        ElfFile f(QStringLiteral(BINDIR "single-executable"));
-        QVERIFY(f.open(QFile::ReadOnly));
-        QCOMPARE(f.isValid(), true);
 
+    void checkLinuxABISection(const ElfFile& f)
+    {
         const auto index = f.indexOfSection(".note.ABI-tag");
         QVERIFY(index > 0);
 
@@ -50,6 +46,40 @@ private slots:
         QCOMPARE(entry->name(), "GNU");
         QVERIFY(entry->isGNUVendorNote());
         QCOMPARE(entry->descriptionSize(), (uint64_t)16);
+    }
+
+    void checkFreeBSDABISection(const ElfFile& f)
+    {
+        const auto index = f.indexOfSection(".note.tag");
+        QVERIFY(index > 0);
+
+        ElfNoteSection *section = f.section<ElfNoteSection>(index);
+        QVERIFY(section);
+
+        QVERIFY(section->entryCount() > 0);
+        const auto entry = section->entry(0);
+
+        QVERIFY(entry);
+        QCOMPARE(entry->section(), section);
+        QVERIFY(entry->size() <= section->size());
+        QCOMPARE(entry->type(), (uint64_t)NT_GNU_ABI_TAG);
+        QCOMPARE(entry->name(), "FreeBSD");
+        QVERIFY(!entry->isGNUVendorNote());
+        QCOMPARE(entry->descriptionSize(), (uint64_t)4);
+    }
+
+private slots:
+    void testABISection()
+    {
+        ElfFile f(QStringLiteral(BINDIR "single-executable"));
+        QVERIFY(f.open(QFile::ReadOnly));
+        QCOMPARE(f.isValid(), true);
+
+#ifdef Q_OS_FREEBSD
+        checkFreeBSDABISection(f);
+#else
+        checkLinuxABISection(f);
+#endif
     }
 };
 
