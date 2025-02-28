37cfa2f22a85ca8f6d327a9e7d1b8a98b7d7e741
Volker Krause <vkrause@kde.org>
2025-02-28 17:07:53 +0100

    Include libdwarf in Flatpak
    Also, rewrite FindDwarf.cmake to use pkg-config files and require a much
    newer minimum version that doesn't require the old libelf workarounds
    anymore.
    
    Actual use is still disabled as we still have a bit of porting to newer
    API to do for that to work.

--- cmake/FindDwarf.cmake
+++ cmake/FindDwarf.cmake
@@ -1,56 +1,38 @@
-include(CheckCSourceCompiles)
-include(CMakePushCheckState)
+# SPDX-FileCopyrightText: 2019 Volker Krause <vkrause@kde.org>
+# SPDX-License-Identifier: BSD-2-Clause
 
-find_path(Dwarf_INCLUDE_DIR libdwarf.h PATH_SUFFIXES libdwarf)
+find_package(PkgConfig QUIET)
+pkg_check_modules(PKG_dwarf QUIET libdwarf)
 
-find_library(Dwarf_LIBRARY NAMES dwarf)
+set(Dwarf_VERSION ${PKG_dwarf_VERSION})
 
-# test if libdwarf requires libelf to link
-cmake_push_check_state()
-set(CMAKE_REQUIRED_INCLUDES ${Dwarf_INCLUDE_DIR})
-set(CMAKE_REQUIRED_LIBRARIES ${Dwarf_LIBRARY})
-check_c_source_compiles("
-#include <libdwarf.h>
-int main(int argc, char** argv) {
-    Dwarf_Die die;
-    return dwarf_tag(die, 0, 0);
-}" DWARF_LINKS_WITHOUT_LIBELF)
+find_path(Dwarf_INCLUDE_DIR
+    NAMES libdwarf.h
+    PATH_SUFFIXES "libdwarf" "libdwarf-0"
+    HINTS ${PKG_dwarf_INCLUDEDIR} ${PKG_dwarf_INCLUDE_DIR}
+)
 
-if(NOT DWARF_LINKS_WITHOUT_LIBELF)
-    find_library(elf_LIBRARY NAMES elf)
-    if (NOT elf_LIBRARY)
-       message(WARNING "elf library not found")
-    endif()
-    set(CMAKE_REQUIRED_LIBRARIES ${Dwarf_LIBRARY} ${elf_LIBRARY})
-    check_c_source_compiles("
-    #include <libdwarf.h>
-    int main(int argc, char** argv) {
-        Dwarf_Die die;
-        return dwarf_tag(die, 0, 0);
-    }" DWARF_NEEDS_LIBELF)
-endif()
-cmake_pop_check_state()
-
-if(NOT DWARF_LINKS_WITHOUT_LIBELF AND NOT DWARF_NEEDS_LIBELF)
-    message(WARNING "Can't get libdwarf to link!?")
-    return()
-endif()
+find_library(Dwarf_LIBRARY
+    NAMES dwarf
+    HINTS ${PKG_dwarf_LIBDIR} ${PKG_dwarf_LIBRARY_DIRS}
+)
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(Dwarf DEFAULT_MSG Dwarf_LIBRARY Dwarf_INCLUDE_DIR)
+find_package_handle_standard_args(Dwarf
+    FOUND_VAR Dwarf_FOUND
+    REQUIRED_VARS Dwarf_LIBRARY Dwarf_INCLUDE_DIR
+    VERSION_VAR Dwarf_VERSION
+)
 
-if(DWARF_FOUND AND NOT TARGET Dwarf::Dwarf)
+if(Dwarf_FOUND AND NOT TARGET Dwarf::Dwarf)
     add_library(Dwarf::Dwarf UNKNOWN IMPORTED)
     set_target_properties(Dwarf::Dwarf PROPERTIES
         IMPORTED_LOCATION "${Dwarf_LIBRARY}"
         INTERFACE_INCLUDE_DIRECTORIES "${Dwarf_INCLUDE_DIR}"
     )
-    if(DWARF_NEEDS_LIBELF)
-        set_target_properties(Dwarf::Dwarf PROPERTIES INTERFACE_LINK_LIBRARIES ${elf_LIBRARY})
-    endif()
 endif()
 
-mark_as_advanced(Dwarf_LIBRARY Elf_LIBRARY Dwarf_INCLUDE_DIR)
+mark_as_advanced(Dwarf_LIBRARY Dwarf_INCLUDE_DIR Dwarf_VERSION)
 
 include(FeatureSummary)
 set_package_properties(libdwarf-devel PROPERTIES URL http://www.dwarfstd.org/
